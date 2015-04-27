d79435c76391a05aae9148199eb57965162c3915
root <root@Zoidepomba.com.br>
2015-04-27 19:32:07 -0400

    Testes com http/2

--- index.js
+++ index.js
@@ -198,9 +198,11 @@ app.get('/contact', function(req, res){
     });
 });
 
-var server = app.listen(3000, function(){
+http.createServer(app).listen(3000);
+
+/*var server = app.listen(3000, function(){
   var host = server.address().address;
   var port = server.address().port;
 
   console.log("Running on  port http://%s:%s",host, port);
-});
+});*/
